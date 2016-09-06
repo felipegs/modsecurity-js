2c7d629737480ac26663d6b04fa3fb1e0b79e4fb
Felipe Santos <felipegs@gmail.com>
2016-09-06 14:42:35 -0300

    removing console.log

--- spec/modsecurity-spec.js
+++ spec/modsecurity-spec.js
@@ -11,8 +11,6 @@ describe('Modsecurity', () => {
       const data = {"clientIP": "127.0.0.1", "clientPort": 1234, "serverIP": "198.0.0.1", "serverPort": 40, "headers" : {"Content-Type": "application/json"}, "payload": "{ \"ola\": }" }
       const rules = fs.readFileSync(path.join(__dirname, "rules.conf"), "utf8");
       modsecurity.hasThreats(data, rules, function(err, result) {
-        console.log(err)
-        console.log(result)
         expect(result.hasThreats).toBe(true)
         done()
       })
